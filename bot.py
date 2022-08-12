e51a45e7fadfce39da639ff9c3fdb46732497528
Timur Sharafutdinov <sharafutdinovtimurr@gmail.com>
2022-08-12 16:13:26 +0300

    Bot v1

---
+++ bot.py
@@ -0,0 +1,35 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+import settings
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def start_bot(update, context):
+    print("Вызван /start")
+    print(update)
+    update.message.reply_text("Здравствуй, {} Жопка {}. Нюдсы мои ждешь?!".format(update.message.chat.first_name, update.message.chat.last_name))
+
+def chat_me(update, context):
+    text = update.message.text
+    update.message.reply_text(text)
+    logging.info(text)
+
+def main():
+    upd = Updater(settings.TELEGRAM_API_KEY)
+
+    upd.dispatcher.add_handler(CommandHandler('start', start_bot))
+    upd.dispatcher.add_handler(MessageHandler(Filters.text, chat_me))
+
+    upd.start_polling()
+    upd.idle()
+
+
+
+
+if __name__ == "__main__":
+    logging.info("bot started")
+    main()
+     
